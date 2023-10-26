eb53ed8118fefc1aa6d21bd142280baa981409cb
divyang <divyang0024@gmail.com>
2023-10-26 21:21:18 +0530

    some few changes in bg changer project

--- 02color/src/App.jsx
+++ 02color/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive");
 
   return (
     <>
-      <br />
-      <br />
-      <span className='text-shadowgreen-100 bg-pink-500 border-x-4 border-y-8 border-blue-500'>Hello there!!!!.</span>
+      <div className="w-full h-screen duration-1000 ease-in-out" style={{ backgroundColor: color }}></div>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "red" }} onClick={() => setColor("red")}>red</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "green" }} onClick={() => setColor("green")}>green</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "blue" }} onClick={() => setColor("blue")}>blue</button>
+        </div>
+      </div>
     </>
   )
 }
